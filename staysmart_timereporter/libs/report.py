93dcc0d5e2ab3159d4a72148cde396c290fe7926
Jeffrey Santana <jeffrey.santanadejesus@stud.fhgr.ch>
2019-10-24 15:33:16 +0200

    improve code

--- staysmart_timereporter/libs/report.py
+++ staysmart_timereporter/libs/report.py
@@ -28,14 +28,35 @@ def append_members_json(members,key,firstname,lastname,hours,salary):
         }   )
     return members
 
+def append_project_json(projects,name,members,tasks,projecttime,salary):
+    projects[name] = []
+    projects[name].append({
+                    'members': members,
+                    'tasks' : tasks,
+                    'time' : projecttime,
+                    'costs' : round(projecttime *salary,2)
+                    }   )
+    return projects
+
+def append_task_json(tasks,key,name,time):
+    tasks[key] =[]
+    tasks[key].append({
+            'name' : name,
+            'time' : time
+        })
+    return tasks
+
 def sec_to_hours(seconds):
     hours = seconds/3600
     hours = round(hours,2)
     return hours
 
+def get_report_data(selected_projects,apikey):
+    return
+
 def report(selected_projects,salary,apikey):
     projects = {}
-    #projects['projects'].append({'PR&Marketing2019'}) 
+    
     for selected_project in selected_projects:
         name = meistertask.get_projects(selected_project,apikey)
         name = name['name']
@@ -43,8 +64,7 @@ def report(selected_projects,salary,apikey):
         times = meistertask.get_workintervals_project(selected_project,apikey)
         persons = meistertask.get_persons_project(selected_project,apikey)
         project_tasks = meistertask.get_tasks(selected_project,apikey)
-        persons = persons['persons']
-        projects[name] = []
+        persons = persons['persons'] 
         members = {}
         tasks={}
         for person in persons:
@@ -57,22 +77,12 @@ def report(selected_projects,salary,apikey):
             members = append_members_json(members,person['id'],person['firstname'],person['lastname'],worktime,salary)
         for task in project_tasks:
             worktime = 0.0
-            
             worktime= sec_to_hours(task['tracked_time'])
             projecttime = projecttime + worktime
-            key= task['id']
-            tasks[key] =[]
-            tasks[key].append({
-                'name' : task['name'],
-                'time' : worktime
-            })
-
-        projects[name].append({
-                    'members': members,
-                    'tasks' : tasks,
-                    'time' : projecttime,
-                    'costs' : round(projecttime *salary,2)
-                    }   )
+            tasks = append_task_json(tasks,task['id'],task['name'],worktime)
+        projects = append_project_json(projects,name,members,tasks,projecttime,salary)
+     
+        
   
     path = export_report_json(projects)
     return path
