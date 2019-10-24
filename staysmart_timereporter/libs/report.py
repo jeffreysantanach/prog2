0b402cc20f69cab77d4f4bc536144418c658ba2b
Jeffrey Santana <jeffrey.santanadejesus@stud.fhgr.ch>
2019-10-24 07:49:18 +0200

    improve code with function

--- staysmart_timereporter/libs/report.py
+++ staysmart_timereporter/libs/report.py
@@ -1,7 +1,7 @@
 from . import meistertask_requests as meistertask
 from datetime import datetime
 from datetime import timedelta
-from libs import data_helper
+from . import data_helper
 import json
 
 def calctime(starttime,endtime):
@@ -9,10 +9,30 @@ def calctime(starttime,endtime):
     time = endtime - starttime
     time = time.seconds
     return time
+
+def export_report_json(data):
+    now = datetime.now()
+    dt_string = now.strftime("%d%m%Y%H%M%S")
+    path = 'report' + dt_string + '.json'
+    path_json = 'data/' +path
+    data_helper.save_json(path_json,data)
+    return path
+
+def append_members_json(members,key,firstname,lastname,hours,salary):
+    members[key] = []
+    members[key].append({
+       'firstname' : firstname,
+       'lastname' : lastname,
+       'hours' : round(hours,2),
+       'salary' : round(hours*salary,2)
+        }   )
+    return members
+
 def sec_to_hours(seconds):
     hours = seconds/3600
     hours = round(hours,2)
     return hours
+
 def report(selected_projects,salary,apikey):
     projects = {}
     #projects['projects'].append({'PR&Marketing2019'}) 
@@ -33,16 +53,8 @@ def report(selected_projects,salary,apikey):
                 if time['person_id'] == person['id']:
                     result= float(calctime(datetime.strptime(time['started_at'],'%Y-%m-%dT%H:%M:%S.%fZ'),datetime.strptime(time['finished_at'],'%Y-%m-%dT%H:%M:%S.%fZ')))
                     result = sec_to_hours(result)
-                    worktime = worktime + result     
-            key = person['id']  
-            members[key] = []
-            members[key].append({
-                'firstname' : person['firstname'],
-                'lastname' : person['lastname'],
-                'hours' : worktime,
-                'salary' : round(worktime *salary,2)
-                    
-                }) 
+                    worktime = worktime + result  
+            members = append_members_json(members,person['id'],person['firstname'],person['lastname'],worktime,salary)
         for task in project_tasks:
             worktime = 0.0
             
@@ -61,14 +73,6 @@ def report(selected_projects,salary,apikey):
                     'time' : projecttime,
                     'costs' : round(projecttime *salary,2)
                     }   )
-    now = datetime.now()
-    dt_string = now.strftime("%d%m%Y%H%M%S")
-    path = 'report' + dt_string + '.json'
-    path_json = 'data/' +path
-    """
-    data_helper.save_json(path_json,projects)
-    """
-    with open(path_json,'w') as outfile:
-        json.dump(projects,outfile,indent=4)
   
+    path = export_report_json(projects)
     return path
