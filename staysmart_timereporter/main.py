229a9e914f2a0a45e83ba6d5e2f232d9fd7e5c26
Jeffrey Santana <jeffsant.js@gmail.com>
2019-12-02 17:11:21 +0100

    add oauth2 authentication

--- staysmart_timereporter/main.py
+++ staysmart_timereporter/main.py
@@ -1,6 +1,7 @@
 # import modules
 import os
 import json
+import requests
 from flask import Flask
 from flask import render_template, request, redirect, url_for,send_file
 from flask import send_file, send_from_directory, abort
@@ -15,6 +16,7 @@ app = Flask("staysmart_timereporter")
 csv_path ="data/csv"
 app.config["CLIENT_CSV"] = csv_path
 json_path = "./data/json"
+auth_path = "./data/auth"
 
 #functions
 def get_filepath(path,datatype):
@@ -24,33 +26,28 @@ def get_filepath(path,datatype):
 #route functions
 
 @app.route('/',methods=['GET','POST'])
-def auth():
+def index():
     files = data_helper.get_all_files(json_path)
+    data = data_helper.load_json(auth_path + '/auth.json')
+    client_id = data['client_id']
+    client_secret = data['client_secret']
     if request.method == 'POST' :
-        if request.form['submit'] == 'Search':
-            api_key = request.form['apikey']
-            project_names= data_helper.get_list_of_projects(api_key)
-            if project_names is None:
-                 return render_template("index.html",error= 'true')
-            else:
-                return render_template("index.html",projects= project_names, api_key=api_key)
-        elif request.form['submit'] == 'Submit':
-            api_key = request.form['apikey']
-            
-            try: 
+        if request.form['submit'] == 'Submit':
+          api_key = request.form['apikey']
+          try: 
                 selected_projects =  request.form.getlist('mycheckbox')
                 salary = request.form['salary']
                 memberfee = request.form['memberfee']
                 path = report.report(selected_projects,int(salary),api_key,memberfee)
                 redirect_path = 'projects/' + path
-                return redirect(redirect_path)
-            except:
+                return redirect(redirect_path)   
+          except:
                 return render_template("index.html",error= 'true')
+
         elif request.form['submit'] == 'Report':
             path = 'projects/' + request.form['file']
-            return redirect(path)
-            
-    return render_template("index.html",files= files)
+            return redirect(path)       
+    return render_template("index.html",files= files,client_id=client_id, client_secret=client_secret)
 
 @app.route('/projects/<path>')
 def projects(path):
@@ -100,11 +97,33 @@ def csv_export(path):
     except FileNotFoundError:
        return abort(404)
 
+@app.route('/returnpath',methods=['GET','POST'])
+def auth():
+    if request.method == 'GET':
+        code = request.args.get('code')
+        access_token = login(code)
+        access_token = access_token['access_token']
+        api_key= "Bearer " + access_token
+        project_names= data_helper.get_list_of_projects(api_key)
+        if project_names is None:
+            return render_template("index.html",error= 'true')
+        else:
+            return render_template("index.html",projects= project_names, api_key=api_key)
+        
+
+def login(code):
+    data = data_helper.load_json(json_path+"/auth.json")
+    client_id = data['client_id']
+    client_secret = data['client_secret']
+    url= 'https://www.mindmeister.com/oauth2/token?grant_type=authorization_code&code='+code+'&client_id='+client_id+'&client_secret='+client_secret+'&redirect_uri=https://127.0.0.1:5000/returnpath&scopes=mindmeister'
+    response = requests.post(url)
+    response = response.json()
+    return response
 
 #errorhandler
 @app.errorhandler(404)
 def page_not_found(e):
-    return redirect('http://127.0.0.1:5000/')  
+    return redirect('https://127.0.0.1:5000/')  
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
- #this is a test commit
+    app.run(ssl_context='adhoc',debug=True, port=5000)
+ 
